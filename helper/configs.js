312b55aa174fa7f55d01797cdb2e3f54cb45f5e7
chivy04 <113112886+chivy04@users.noreply.github.com>
2024-01-06 22:26:50 +0700

    tuan8

---
+++ helper/configs.js
@@ -0,0 +1,19 @@
+module.exports= {
+    PLEASUE_LOGIN: "Vui long dang nhap",
+    DSRole: ['admin', 'publisher','user'],
+    NOT_ACCESS: "Bạn không đủ quyền",
+    NOT_FOUND_USER: "Khong tim thay nguoi dung",
+    REGISTER_FAIL_ALREADY_EXIST: "user da ton tai",
+    LOGOUT_SUCCESS: 'logout thanh cong',
+    SEND_MAIL_SUCCESS: 'gui mail thanh cong',
+    SEND_MAIL_FAIL: 'gui mail loi vui long thu lai: %s',
+    RESET_PASSWORD_SUCCESS: 'Thay đổi mật khẩu thành công',
+
+    SECRET_KEY:"2011063573",
+    EXP: '20d',
+    cookieExp: 2,
+    MAIL_HOST:'sandbox.smtp.mailtrap.io',
+    MAIL_PORT:2525,
+    MAIL_USER: '01fb5c341e9fc9',
+    MAIL_PASS: 'd1c8fd912fe438',
+}
---
+++ routes/departments.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+var SchemaDepartment = require('../schema/deparment');
+var responseData = require('../helper/responseData');
+var mongoose = require("mongoose");
+
+router.get('/', async function (req, res, next) {
+  var allDepartment = await SchemaDepartment.find({})
+  .populate({path:'employees',select:'_id userName'});
+  responseData.responseReturn(res, 200, true, allDepartment);
+});
+
+router.post('/Create', async function (req, res, next) {
+    try {
+        const name = req.body.name;
+        console.log(name);
+        const newDepartment = await SchemaDepartment.create({ name });
+
+        responseData.responseReturn(res, 201, true, newDepartment);
+    } catch (error) {
+        console.error('Error creating department:', error);
+        responseData.responseReturn(res, 500, false, 'Internal Server Error');
+    }
+});
+
+router.put('/:id', async function (req, res, next) {
+    try {
+        const departmentId = req.params.id;
+        const { name } = req.body;
+
+        // Kiểm tra xem ID có hợp lệ không
+        if (!mongoose.Types.ObjectId.isValid(departmentId)) {
+            return responseData.responseReturn(res, 400, false, 'Invalid department ID');
+        }
+
+        // Lấy thông tin của bộ phận dựa trên ID
+        const department = await SchemaDepartment.findById(departmentId);
+
+        // Kiểm tra xem bộ phận có tồn tại hay không
+        if (!department) {
+            return responseData.responseReturn(res, 404, false, 'Department not found');
+        }
+
+        // Cập nhật thông tin bộ phận
+        department.name = name;
+        await department.save();
+
+        // Trả về thông tin của bộ phận sau khi cập nhật
+        responseData.responseReturn(res, 200, true, department);
+    } catch (error) {
+        console.error('Error updating department by ID:', error);
+        responseData.responseReturn(res, 500, false, 'Internal Server Error');
+    }
+});
+
+module.exports = router;
