8a1cb1ee0d2b1d7034c88ca695d001d1513bf954
chivy04 <113112886+chivy04@users.noreply.github.com>
2024-01-08 09:40:27 +0700

    pull

--- routes/departments.js
+++ routes/departments.js
@@ -10,6 +10,28 @@ router.get('/', async function (req, res, next) {
   responseData.responseReturn(res, 200, true, allDepartment);
 });
 
+router.get('/:id', async function (req, res, next) {
+    try {
+        const departmentId = req.params.id;
+        if (!mongoose.Types.ObjectId.isValid(departmentId)) {
+            return responseData.responseReturn(res, 400, false, 'Invalid department ID');
+        }
+
+        const department = await SchemaDepartment.findById(departmentId)
+            .populate({ path: 'employees', select: '_id userName' });
+
+        if (!department) {
+            return responseData.responseReturn(res, 404, false, 'Department not found');
+        }
+
+        responseData.responseReturn(res, 200, true, department);
+    } catch (error) {
+        console.error('Error getting department by ID:', error);
+        responseData.responseReturn(res, 500, false, 'Internal Server Error');
+    }
+});
+  
+
 router.post('/Create', async function (req, res, next) {
     try {
         const name = req.body.name;
@@ -28,24 +50,19 @@ router.put('/:id', async function (req, res, next) {
         const departmentId = req.params.id;
         const { name } = req.body;
 
-        // Kiểm tra xem ID có hợp lệ không
         if (!mongoose.Types.ObjectId.isValid(departmentId)) {
             return responseData.responseReturn(res, 400, false, 'Invalid department ID');
         }
 
-        // Lấy thông tin của bộ phận dựa trên ID
         const department = await SchemaDepartment.findById(departmentId);
 
-        // Kiểm tra xem bộ phận có tồn tại hay không
         if (!department) {
             return responseData.responseReturn(res, 404, false, 'Department not found');
         }
 
-        // Cập nhật thông tin bộ phận
         department.name = name;
         await department.save();
 
-        // Trả về thông tin của bộ phận sau khi cập nhật
         responseData.responseReturn(res, 200, true, department);
     } catch (error) {
         console.error('Error updating department by ID:', error);
@@ -53,4 +70,25 @@ router.put('/:id', async function (req, res, next) {
     }
 });
 
+router.delete('/:id', async function (req, res, next) {
+    try {
+      const departmentId = req.params.id;
+  
+      if (!mongoose.Types.ObjectId.isValid(departmentId)) {
+        return responseData.responseReturn(res, 400, false, 'Invalid department ID');
+      }
+      console.log(departmentId);
+      const department = await SchemaDepartment.findOneAndDelete({ _id: departmentId });
+  
+      if (!department) {
+        return responseData.responseReturn(res, 404, false, 'Department not found');
+      }
+  
+      responseData.responseReturn(res, 200, true, 'Department deleted successfully');
+    } catch (error) {
+      console.error('Error deleting department by ID:', error);
+      responseData.responseReturn(res, 500, false, 'Internal Server Error');
+    }
+  });  
+  
 module.exports = router;
